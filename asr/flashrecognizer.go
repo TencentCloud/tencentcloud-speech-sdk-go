a54edf49392ee57fc05a5d2a8350aac7808bbd6d
jianfeiyun <jianfeiyun@tencent.com>
2021-01-01 11:40:18 +0800

    add flash recognizer

---
+++ asr/flashrecognizer.go
@@ -0,0 +1,210 @@
+package asr
+
+import (
+	"bytes"
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"sort"
+	"strconv"
+	"time"
+
+	"github.com/tencentcloud/tencentcloud-speech-sdk-go/common"
+)
+
+// FlashRecognitionRequest FlashRecognitionRequest
+type FlashRecognitionRequest struct {
+	EngineType         string `json:"engine_type"`
+	VoiceFormat        string `json:"voice_format"`
+	SpeakerDiarization uint32 `json:"speaker_diarization"`
+	HotwordId          string `json:"hotword_id"`
+	FilterDirty        int32  `json:"filter_dirty"`
+	FilterModal        int32  `json:"filter_modal"`
+	FilterPunc         int32  `json:"filter_punc"`
+	ConvertNumMode     int32  `json:"convert_num_mode"`
+	WordInfo           int32  `json:"word_info"`
+	FirstChannelOnly   int32  `json:"first_channel_only"`
+}
+
+// FlashRecognitionResponse FlashRecognitionResponse
+type FlashRecognitionResponse struct {
+	RequestId     string                    `json:"request_id"`
+	Code          int                       `json:"code"`
+	Message       string                    `json:"message"`
+	AudioDuration int64                     `json:"audio_duration"`
+	FlashResult   []*FlashRecognitionResult `json:"flash_result,omitempty"`
+}
+
+// FlashRecognitionResult FlashRecognitionResult
+type FlashRecognitionResult struct {
+	Text         string                      `json:"text"`
+	ChannelId    int32                       `json:"channel_id"`
+	SentenceList []*FlashRecognitionSentence `json:"sentence_list,omitempty"`
+}
+
+// FlashRecognitionSentence FlashRecognitionSentence
+type FlashRecognitionSentence struct {
+	Text      string           `json:"text"`
+	StartTime uint32           `json:"start_time"`
+	EndTime   uint32           `json:"end_time"`
+	SpeakerId int32            `json:"speaker_id"`
+	WordList  []*FlashWordData `json:"word_list,omitempty"`
+}
+
+// FlashWordData FlashWordData
+type FlashWordData struct {
+	Word       string `json:"word"`
+	StartTime  uint32 `json:"start_time"`
+	EndTime    uint32 `json:"end_time"`
+	StableFlag uint32 `json:"stable_flag"`
+}
+
+// newFlashRecognitionResponse newFlashRecognitionResponse
+func newFlashRecognitionResponse(code int, message string) *FlashRecognitionResponse {
+	return &FlashRecognitionResponse{
+		Code:    code,
+		Message: message,
+	}
+}
+
+var (
+	flashHost  = "asr.cloud.tencent.com"
+	httpClient *http.Client
+
+	connTimeout         = 1
+	rwTimeout           = 600
+	maxIdleConns        = 100
+	maxIdleConnsPerHost = 2
+	idleConnTimeout     = time.Duration(180) * time.Second
+)
+
+// init init http client
+func init() {
+	transport := &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			Timeout:   time.Duration(connTimeout) * time.Second,
+			KeepAlive: time.Duration(rwTimeout*10) * time.Second,
+			DualStack: true,
+		}).DialContext,
+		MaxIdleConns:          maxIdleConns,
+		IdleConnTimeout:       idleConnTimeout,
+		TLSHandshakeTimeout:   time.Duration(connTimeout) * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+	}
+	httpClient = new(http.Client)
+	httpClient.Transport = transport
+	httpClient.Timeout = time.Duration(rwTimeout) * time.Second
+}
+
+// FlashRecognizer is the entry for ASR flash recognizer
+type FlashRecognizer struct {
+	AppID      string
+	Credential *common.Credential
+}
+
+// NewFlashRecognizer creates instance of FlashRecognizer
+func NewFlashRecognizer(appID string, credential *common.Credential) *FlashRecognizer {
+	return &FlashRecognizer{
+		AppID:      appID,
+		Credential: credential,
+	}
+}
+
+// DoRecognize  DoRecognize
+func (recognizer *FlashRecognizer) DoRecognize(req *FlashRecognitionRequest,
+	videoData []byte) (*FlashRecognitionResponse, error) {
+
+	signStr, reqUrl := recognizer.buildURL(req)
+	signature := recognizer.genSignature(signStr)
+
+	headers := make(map[string]string)
+	headers["Host"] = flashHost
+	headers["Authorization"] = signature
+
+	httpReq, err := http.NewRequest("POST", reqUrl, bytes.NewReader(videoData))
+	if err != nil {
+		return nil, fmt.Errorf("failed create http request, error: %s", err.Error())
+	}
+	for k, v := range headers {
+		httpReq.Header.Set(k, v)
+	}
+	httpResp, err := httpClient.Do(httpReq)
+	if err != nil {
+		return nil, fmt.Errorf("failed do request, error: %s", err.Error())
+	}
+	defer httpResp.Body.Close()
+	respData, err := ioutil.ReadAll(httpResp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed read body, error: %s", err.Error())
+	}
+	if httpResp.StatusCode != 200 {
+		return nil, fmt.Errorf("http code not 200, respData: %s", string(respData))
+	}
+	resp := &FlashRecognitionResponse{}
+	err = json.Unmarshal(respData, &resp)
+	if err != nil {
+		return nil, fmt.Errorf("failed unmarshal, respData: %s, error: %s", respData, err.Error())
+	}
+	if resp.Code != 0 {
+		return resp, fmt.Errorf("request_id: %s, code: %d, message: %s", resp.RequestId, resp.Code, resp.Message)
+	}
+	return resp, nil
+}
+
+// buildURL buildURL
+func (recognizer *FlashRecognizer) buildURL(req *FlashRecognitionRequest) (string, string) {
+	var queryMap = make(map[string]string)
+	queryMap["secretid"] = recognizer.Credential.SecretId
+	queryMap["engine_type"] = req.EngineType
+	queryMap["voice_format"] = req.VoiceFormat
+	queryMap["speaker_diarization"] = strconv.FormatInt(int64(req.SpeakerDiarization), 10)
+	queryMap["hotword_id"] = req.HotwordId
+	queryMap["filter_dirty"] = strconv.FormatInt(int64(req.FilterDirty), 10)
+	queryMap["filter_modal"] = strconv.FormatInt(int64(req.FilterModal), 10)
+	queryMap["filter_punc"] = strconv.FormatInt(int64(req.FilterPunc), 10)
+	queryMap["convert_num_mode"] = strconv.FormatInt(int64(req.ConvertNumMode), 10)
+	queryMap["word_info"] = strconv.FormatInt(int64(req.WordInfo), 10)
+	queryMap["first_channel_only"] = strconv.FormatInt(int64(req.FirstChannelOnly), 10)
+	var timestamp = time.Now().Unix()
+	var timestampStr = strconv.FormatInt(timestamp, 10)
+	queryMap["timestamp"] = timestampStr
+
+	var keys []string
+	for k := range queryMap {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	var queryStrBuffer bytes.Buffer
+	for _, k := range keys {
+		queryStrBuffer.WriteString(k)
+		queryStrBuffer.WriteString("=")
+		queryStrBuffer.WriteString(queryMap[k])
+		queryStrBuffer.WriteString("&")
+	}
+
+	rs := []rune(queryStrBuffer.String())
+	rsLen := len(rs)
+	queryStr := string(rs[0 : rsLen-1])
+
+	url := fmt.Sprintf("%s/asr/flash/v1/%s?%s", flashHost, recognizer.AppID, queryStr)
+	signStr := fmt.Sprintf("POST%s", url)
+	reqUrl := fmt.Sprintf("https://%s", url)
+	return signStr, reqUrl
+}
+
+// genSignature genSignature
+func (recognizer *FlashRecognizer) genSignature(url string) string {
+	hmac := hmac.New(sha1.New, []byte(recognizer.Credential.SecretKey))
+	signURL := url
+	hmac.Write([]byte(signURL))
+	encryptedStr := hmac.Sum([]byte(nil))
+	var signature = base64.StdEncoding.EncodeToString(encryptedStr)
+	return signature
+}
---
+++ examples/asrexample/flashexample.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/tencentcloud/tencentcloud-speech-sdk-go/asr"
+	"github.com/tencentcloud/tencentcloud-speech-sdk-go/common"
+)
+
+var (
+	// AppID AppID
+	AppID = "AppID"
+	// SecretID SecretID
+	SecretID = "SecretID"
+	// SecretKey SecretKey
+	SecretKey = "SecretKey"
+	// EngineType EngineType
+	EngineType = "16k_zh"
+)
+
+func main() {
+	var c = flag.Int("c", 1, "concurrency")
+	var l = flag.Bool("l", false, "loop or not")
+	var f = flag.String("f", "test.pcm", "audio file")
+	flag.Parse()
+
+	var wg sync.WaitGroup
+	for i := 0; i < *c; i++ {
+		fmt.Println("Main: Starting worker", i)
+		wg.Add(1)
+		if *l {
+			go processLoop(i, &wg, *f)
+		} else {
+			go processOnce(i, &wg, *f)
+		}
+	}
+
+	fmt.Println("Main: Waiting for workers to finish")
+	wg.Wait()
+	fmt.Println("Main: Completed")
+
+}
+
+func processLoop(id int, wg *sync.WaitGroup, file string) {
+	defer wg.Done()
+	for {
+		process(id, file)
+	}
+}
+
+func processOnce(id int, wg *sync.WaitGroup, file string) {
+	defer wg.Done()
+	process(id, file)
+}
+
+func process(id int, file string) {
+	audio, err := os.Open(file)
+	defer audio.Close()
+	if err != nil {
+		fmt.Printf("open file error: %v\n", err)
+		return
+	}
+	credential := common.NewCredential(SecretID, SecretKey)
+	recognizer := asr.NewFlashRecognizer(AppID, credential)
+	data, err := ioutil.ReadAll(audio)
+	if err != nil {
+		fmt.Printf("%s|failed read data, error: %v\n", time.Now().Format("2006-01-02 15:04:05"), err)
+		return
+	}
+
+	req := new(asr.FlashRecognitionRequest)
+	req.EngineType = EngineType
+	req.VoiceFormat = "wav"
+	req.SpeakerDiarization = 0
+	req.FilterDirty = 0
+	req.FilterModal = 0
+	req.FilterPunc = 0
+	req.ConvertNumMode = 1
+	req.FirstChannelOnly = 1
+	req.WordInfo = 0
+
+	resp, err := recognizer.DoRecognize(req, data)
+	if err != nil {
+		fmt.Printf("%s|failed do recognize, error: %v\n", time.Now().Format("2006-01-02 15:04:05"), err)
+		return
+	}
+	fmt.Printf("request_id: %s\n", resp.RequestId)
+
+	for _, channelResult := range resp.FlashResult {
+		fmt.Printf("channel_id: %d, result: %s\n", channelResult.ChannelId, channelResult.Text)
+	}
+}
